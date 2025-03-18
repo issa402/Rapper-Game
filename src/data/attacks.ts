dd1fd6d6defa0d2b372e659120c124f0af4910be
issa402 <65190332+issa402@users.noreply.github.com>
2025-03-17 23:47:57 -0400

    Add attack moves data

---
+++ src/data/attacks.ts
@@ -0,0 +1,20 @@
+import { Attack } from '../types/gameTypes';
+
+export const attacks: Attack[] = [
+  {
+    name: '🎵 Freestyle Flow',
+    damage: 15
+  },
+  {
+    name: '🔥 Sick Burn',
+    damage: 25
+  },
+  {
+    name: '💫 Epic Wordplay',
+    damage: 20
+  },
+  {
+    name: '⚡ Ultimate Diss',
+    damage: 30
+  }
+];
